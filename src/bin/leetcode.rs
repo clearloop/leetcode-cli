bc38fd064c45968894a14f66dc6e6e49936daeb1
clearloop <udtrokia@gmail.com>
2019-12-30 08:50:58 +0800

    add clap support

--- src/bin/leetcode.rs
+++ src/bin/leetcode.rs
@@ -1,9 +1,45 @@
-use leetcode_cli::plugins::leetcode;
+extern crate clap;
+use clap::{
+    App, Arg, AppSettings, SubCommand,
+};
 
 fn main() {
-    env_logger::init();
-    
-    let lc = leetcode::LeetCode::new();
-    let mut res = lc.get_category_problems("database");
-    println!("{:#?}", res.text());
+    let m = App::new("leetcode")
+        .author("clearloop <udtrokia@163.com>")
+        .version("0.1.0")
+        .about("Leet your code in command-line.")
+        .subcommand(
+            SubCommand::with_name("list")
+                .about("List problems")
+                .arg(Arg::with_name("all")
+                     .short("w")
+                     .long("all")
+                     .help("List all problems")
+                     .display_order(1)
+                )
+                .arg(Arg::with_name("algorithm")
+                     .short("a")
+                     .long("algorithm")
+                     .help("List algorithm problems")
+                     .display_order(2)
+                )
+                .arg(Arg::with_name("database")
+                     .short("d")
+                     .long("database")
+                     .help("List database problems")
+                     .display_order(3)
+                )
+                .arg(Arg::with_name("shell")
+                     .short("s")
+                     .long("shell")
+                     .help("List shell problems")
+                     .display_order(4)
+                )
+        )
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .get_matches();
+
+    if let Some(m) = m.subcommand_matches("list") {
+        println!("{:?}", m);
+    }
 }
