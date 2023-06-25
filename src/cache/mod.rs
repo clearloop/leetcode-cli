5e466d5b01bd7ab67ec1b9e0d6c8ec5c8fa9f69b
Saksham Mittal <gotlouemail@gmail.com>
2023-06-25 20:50:19 +0530

    Fix rebasing conflicts

--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -122,6 +122,19 @@ impl Cache {
         Ok(p)
     }
 
+    /// Get problem from name
+    pub fn get_problem_from_name(&self, problem_name: &String) -> Result<Problem, Error> {
+        let p: Problem = problems
+            .filter(name.eq(problem_name))
+            .first(&self.conn()?)?;
+        if p.category != "algorithms" {
+            return Err(Error::FeatureError(
+                "Not support database and shell questions for now".to_string(),
+            ));
+        }
+        Ok(p)
+    }
+
     /// Get daily problem
     pub async fn get_daily_problem_id(&self) -> Result<i32, Error> {
         parser::daily(
