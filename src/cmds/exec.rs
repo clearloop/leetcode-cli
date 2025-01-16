0eebee8555e9ce13faa273ddec1bf5b36997056c
Dzuchun <Infernodambik@gmail.com>
2025-01-16 05:00:14 +0200

    add `--daily` cli option for `e`, `t` and `x`

--- src/cmds/exec.rs
+++ src/cmds/exec.rs
@@ -2,7 +2,7 @@
 use super::Command;
 use crate::{Error, Result};
 use async_trait::async_trait;
-use clap::{Arg, ArgMatches, Command as ClapCommand};
+use clap::{Arg, ArgAction, ArgGroup, ArgMatches, Command as ClapCommand};
 
 /// Abstract Exec Command
 ///
@@ -36,14 +36,40 @@ impl Command for ExecCommand {
                     .value_parser(clap::value_parser!(i32))
                     .help("question id"),
             )
+            .arg(
+                Arg::new("daily")
+                    .short('d')
+                    .long("daily")
+                    .help("Edit today's daily challenge")
+                    .action(ArgAction::SetTrue),
+            )
+            .group(
+                ArgGroup::new("question-id")
+                    .args(["id", "daily"])
+                    .multiple(false)
+                    .required(true),
+            )
     }
 
     /// `exec` handler
     async fn handler(m: &ArgMatches) -> Result<()> {
         use crate::cache::{Cache, Run};
 
-        let id: i32 = *m.get_one::<i32>("id").ok_or(Error::NoneError)?;
         let cache = Cache::new()?;
+
+        let daily = m.get_one::<bool>("daily").unwrap_or(&false);
+        let daily_id = if *daily {
+            Some(cache.get_daily_problem_id().await?)
+        } else {
+            None
+        };
+
+        let id = m
+            .get_one::<i32>("id")
+            .copied()
+            .or(daily_id)
+            .ok_or(Error::NoneError)?;
+
         let res = cache.exec_problem(id, Run::Submit, None).await?;
 
         println!("{}", res);
